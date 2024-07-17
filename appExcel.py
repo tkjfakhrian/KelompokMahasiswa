0dc93c59617ac5381e819bd721eef51276ba702e
Fakhrian Fadlia A <tkj.fakhrian@gmail.com>
2024-07-17 11:58:52 +0700

    Update appExcel.py
    Penambahan Fitur Representasi Pengetahuan Menggunakan Grafik Pie

--- appExcel.py
+++ appExcel.py
@@ -17,6 +17,7 @@ class MainClass() :
     def __init__(self):
         self.data = Data()
         self.kelompok = Kelompok()
+        self.representasi = Representasi()
 
     # Fungsi judul halaman
     def judul_halaman(self):
@@ -26,8 +27,8 @@ class MainClass() :
     # Fungsi menu sidebar
     def sidebar_menu(self):
         with st.sidebar:
-            selected = option_menu('Menu',['Upload Data','Kelompok Mahasiswa'],
-            icons =["easel2", "table"],
+            selected = option_menu('Menu',['Upload Data','Kelompok Mahasiswa','Representasi Kelompok'],
+            icons =["easel2", "table","paperclip"],
             menu_icon="cast",
             default_index=0)
             
@@ -36,6 +37,9 @@ class MainClass() :
 
         if (selected == 'Kelompok Mahasiswa'):
             self.kelompok.kelompok_mhs()
+
+        if (selected == 'Representasi Kelompok') :
+            self.representasi.kelompok_mhs()
 
 class Data(MainClass) :
     def __init__(self):
@@ -57,104 +61,29 @@ class Data(MainClass) :
     def tampil_DataMahasiswa(self) :
         if not self.state['DataMahasiswa'].empty:
             Data = self.state['DataMahasiswa']
+            Data['NIM'] = Data['NIM'].astype(str)
             st.dataframe(Data)
 
     def menu_data(self):
         self.judul_halaman()
         self.upload_DataMahasiswa()
         self.tampil_DataMahasiswa()
-
-class DBI(Data) :
-    def __init__(self) :
-        self.state = st.session_state.setdefault('state', {})
-        if 'DataPenerimaan' not in self.state:
-            self.state['DataPenerimaan'] = pd.DataFrame()
-
-    def RekomendasiDBI(self) :
-        self.judul_halaman()
-        #try :
-            #DataPenerimaan = self.state['DataPenerimaan'] 
-            #results = {}
-            #for i in range(2,8) :
-                #hc = AgglomerativeClustering(n_clusters=i, affinity='euclidean',linkage='ward')
-                #y_hc = hc.fit_predict(DataPenerimaan)
-                #db_index = davies_bouldin_score(DataPenerimaan,y_hc)
-                #results.update({i:db_index})
-
-            #convert dictionary to DataFrame
-            #df = pd.DataFrame(list(results.items()), columns=['Jml_Clusters','Nilai_DBI'])
-
-            #convert 'X' to numeric
-            #df['Jml_Clusters'] = pd.to_numeric(df['Jml_Clusters'])
-
-            #Display the DataFrame
-            #st.markdown(
-            #    """
-            #    <style>
-            #    .dataframe tbody tr th, .dataframe tbody tr td {
-            #        text-align: center;
-            #    }
-            #    </style>
-            #    """,
-            #    unsafe_allow_html=True
-            #)
-            #st.table(df)
-
-            #Mencari Nilai Terkecil
-            #smallest_value = df['Nilai_DBI'].min()
-            #second_smallest_value = df['Nilai_DBI'].nsmallest(2).iloc[-1]
-
-            #Mencari corresponding 'Jml_Clusters' values
-            #smallest_x = df[df['Nilai_DBI'] == smallest_value]['Jml_Clusters'].values[0]
-            #second_smallest_x = df[df['Nilai_DBI'] == second_smallest_value]['Jml_Clusters'].values[0]
-
-            #self.state['smallest_x'] = smallest_x
-            #self.state['second_smallest_x'] = second_smallest_x
-
-            #Create Line Plot
-            #fig,ax = plt.subplots()
-            #ax.plot(df['Jml_Clusters'], df['Nilai_DBI'], marker='o')
-            #ax.set_xlabel('Jumlah Clusters')
-            #ax.set_ylabel('Nilai Davies-Boulding Index')
-            #ax.set_title('Grafik Rekomendasi DBI')
-
-            #Highlight nilai terkecil dan nilai terkecil kedua
-            #ax.plot(smallest_x,smallest_value,marker='o',markersize=10,color='red',label='Jml Cluster Terbaik Ke-1')
-            #ax.plot(second_smallest_x,second_smallest_value,marker='o',markersize=10,color='orange',label='Jml Cluster Terbaik Ke-2')
-            #ax.legend()
-
-            #display plot
-            #st.pyplot(fig)
-
-            #Kesimpulan
-            #st.write("### Kesimpulan :")
-            #st.write(f"- Rekomendasi Kelompok Ke-1 Memiliki Nilai DBI : **{smallest_value:.5f}**, Sehingga Konten Promosi Dapat Dibuat Sebanyak : **{smallest_x}** Konten Promosi")
-            #st.write(f"- Rekomendasi Kelompok Ke-2 Memiliki Nilai DBI : **{second_smallest_value:.5f}** Sehingga Konten Promosi Dapat Dibuat Sebanyak : **{second_smallest_x}** Konten Promosi")
-
-
-        #except :
-            #st.write('Upload File Terlebih Dahulu')
-
+        
 class Kelompok(Data) :
     def __init__(self) :
         self.state = st.session_state.setdefault('state', {})
-        if 'DataPenerimaan' not in self.state:
-            self.state['DataPenerimaan'] = pd.DataFrame()
+        if 'DataMahasiswa' not in self.state :
+            self.state['DataMahasiswa'] = pd.DataFrame()
+
+        if 'Jml_Cluster' not in self.state :
+            self.state['Jml_Cluster'] = 0
 
         if 'smallest_x' not in self.state :
             self.state['smallest_x'] = '-'
             self.state['second_smallest_x'] = '-'
         
-    def to_excel(self,df) :
-        output = BytesIO()
-        writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        df.to_excel(writer, index=False, sheet_name='Kelompok Mahasiswa')
-        writer.close()
-        processed_data = output.getvalue()
-        return processed_data
-
     def representasi_kelompok(self,df,kelompok) :
-        anggota_kelas = df[df['cluster'] == kelompok]
+        anggota_kelas = df[df['cluster'] == kelompok + 1]
 
         #Karakteristik Dari Anggota Kelas
         st.write(f"- Kelompok : **{kelompok+1}**, Memiliki Anggota Sebanyak : **{len(anggota_kelas)}**")
@@ -180,48 +109,132 @@ class Kelompok(Data) :
             cuti = anggota_kelas['Jml_Cuti'].value_counts()
             st.dataframe(cuti.head())
 
+    def KMeans(self,DataMahasiswa,Jml_Cluster) :
+        data_encoded = pd.get_dummies(DataMahasiswa[['IPK', 'SKS', 'Jml_SMS_Saat_Ini', 'Jml_Cuti', 'Tipe_Sekolah']])
+        
+        #Standarisasi Data
+        scaler = StandardScaler()
+        scaled_data = scaler.fit_transform(data_encoded)
+
+        #Inisialisasi Model K-Means
+        kmeans = KMeans(n_clusters=Jml_Cluster,random_state=42)
+                    
+        #Membuat Model K-Means
+        kmeans.fit(scaled_data)
+
+        #Mendapatkan label cluster
+        labels = kmeans.labels_ + 1
+
+        #Menambahkan label cluster ke dataframe asli
+        DataMahasiswa['cluster'] = labels
+
+        #Encode
+        DataMahasiswa['Tipe_Sekolah'] = DataMahasiswa['Tipe_Sekolah'].replace([1,2,3],['Lainnya (MA/Pesantren/homeshooling)','SMK','SMA Umum/PGRI/Plus'])
+
     def kelompok_mhs(self) :
         self.judul_halaman()
         try :
             DataMahasiswa = self.state['DataMahasiswa']
-            data_encoded = pd.get_dummies(DataMahasiswa[['IPK', 'SKS', 'Jml_SMS_Saat_Ini', 'Jml_Cuti', 'Tipe_Sekolah']]) 
+            #data_encoded = pd.get_dummies(DataMahasiswa[['IPK', 'SKS', 'Jml_SMS_Saat_Ini', 'Jml_Cuti', 'Tipe_Sekolah']]) 
 
-            jml_cluster = int(st.number_input('Masukkan Jumlah Kelompok Yang Akan Dibentuk : '))
-            if st.button('Simulasikan') and jml_cluster > 0:
-                
-                #Standarisasi Data
-                scaler = StandardScaler()
-                scaled_data = scaler.fit_transform(data_encoded)
-
-                #Inisialisasi Model K-Means
-                kmeans = KMeans(n_clusters=jml_cluster,random_state=42)
-                
-                #Membuat Model K-Means
-                kmeans.fit(scaled_data)
-
-                #Mendapatkan label cluster
-                labels = kmeans.labels_
-
-                #Menambahkan label cluster ke dataframe asli
-                DataMahasiswa['cluster'] = labels
-
-                #Encode
-                DataMahasiswa['Tipe_Sekolah'] = DataMahasiswa['Tipe_Sekolah'].replace([1,2,3],['Lainnya (MA/Pesantren/homeshooling)','SMK','SMA Umum/PGRI/Plus'])
-
-                st.dataframe(DataMahasiswa)
-                #Download Ke Excel
-                excel_data = self.to_excel(DataMahasiswa)
-                st.download_button(label='Unduh Excel', data=excel_data, file_name='Hasil_Clustering_Mahasiswa.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-
-                #Representasi Pengetahuan
-                for i in range(jml_cluster) :
-                    self.representasi_kelompok(DataMahasiswa,i)
-
+            if self.state['Jml_Cluster'] <= 0 :
+                Jml_Cluster = int(st.number_input('Masukkan Jumlah Kelompok Yang Akan Dibentuk : '))
+                self.state['Jml_Cluster'] = Jml_Cluster
+                if st.button('Bentuk Kelompok') and self.state['Jml_Cluster'] > 0:
+                    self.KMeans(DataMahasiswa,self.state['Jml_Cluster'])
+                    self.state['Representasi'] = False
+                else :
+                    st.write('Mohon Masukkan Jumlah Kelompok Terlebih Dahulu')
             else :
-                st.write('Mohon Masukkan Jumlah Simulasi Terlebih Dahulu')
+                st.write(f"Jumlah Kelompok Yang Dipilih : **{self.state['Jml_Cluster']}**")
+                self.KMeans(DataMahasiswa,self.state['Jml_Cluster'])
+                self.state['Representasi'] = False
 
+            #Representasi Pengetahuan
+            for i in range(self.state['Jml_Cluster']) :
+                self.representasi_kelompok(DataMahasiswa,i)           
+            
         except :
             st.write('Upload File Terlebih Dahulu')         
+
+class Representasi(Data) :
+    def __init__(self) :
+        self.state = st.session_state.setdefault('state', {})
+        if 'DataMahasiswa' not in self.state:
+            self.state['DataMahasiswa'] = pd.DataFrame()
+        
+        if 'Representasi' not in self.state :
+            self.state['Representasi'] = False
+
+    def to_excel(self,df) :
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df.to_excel(writer, index=False, sheet_name='Kelompok Mahasiswa')
+        writer.close()
+        processed_data = output.getvalue()
+        return processed_data
+
+    def representasi_kelompok(self,df,kelompok) :
+        anggota_kelas = df[df['cluster'] == kelompok]
+
+        #Karakteristik Dari Anggota Kelas
+        st.write(f"- Kelompok : **{kelompok}**, Memiliki Anggota Sebanyak : **{len(anggota_kelas)}**")
+
+        #Expander Karakteristik
+        with st.expander(f"Karakteristik Kelompok : **{kelompok}**") :
+            #Rata-rata IPK
+            rata_ipk = anggota_kelas['IPK'].mean()
+            rata_SKS = anggota_kelas['SKS'].mean()
+            st.write(f'Anggota memiliki rata-rata IPK : **{rata_ipk:2.2f}** dengan rata-rata SKS yang sudah diselesaikan **{rata_SKS:2.0f}**')
+
+    def kelompok_mhs(self) :
+        self.judul_halaman()
+        try :
+            DataMahasiswa = self.state['DataMahasiswa']
+            if self.state['Representasi'] == False :
+                #Get Label Cluster
+                unique_clusters = DataMahasiswa['cluster'].unique()
+
+                #Dictionary Untuk Label Cluster Baru
+                new_labels = {}
+                
+                #Loop unique cluster
+                for cluster in unique_clusters :
+                    label_kelompok = st.text_input(f'Masukkan Label Kelompok {cluster}', key=f'label_{cluster}')
+                    new_labels[cluster] = label_kelompok
+
+                #Button untuk update DataFrame
+                if st.button('Represent Kelompok') :
+                    for cluster,label_kelompok in new_labels.items():
+                        if label_kelompok :
+                            DataMahasiswa['cluster'] = DataMahasiswa['cluster'].replace(cluster,label_kelompok)
+                
+                    self.state['Representasi'] = True
+                    self.kelompok_mhs()
+            else :
+                st.header('Proporsi Kelompok')
+                #Pie Chart Proporsi
+                cluster_counts = DataMahasiswa['cluster'].value_counts()
+
+                #Pie Chart
+                fig, ax = plt.subplots()
+                ax.pie(cluster_counts, labels=cluster_counts.index, autopct='%.2f %%', startangle=90)
+                ax.axis('equal') #Equal aspect ratio
+                st.pyplot(fig)
+
+                st.dataframe(DataMahasiswa)
+
+                #Download Ke Excel
+                excel_data = self.to_excel(DataMahasiswa)
+                st.download_button(label='Unduh Hasil Cluster', data=excel_data, file_name='Hasil_Clustering_Mahasiswa.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+                #Get Label Cluster
+                unique_clusters = DataMahasiswa['cluster'].unique()
+
+                for cluster in unique_clusters :
+                    self.representasi_kelompok(DataMahasiswa,cluster) 
+        except :
+            st.write('Upload File Terlebih Dahulu')
 
 if __name__ == "__main__":
     # Create an instance of the main class
